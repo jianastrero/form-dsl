e01316b966bcaf445ebc6dda84d83a4b5d4e1349
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-18 19:57:28 +0800

    📚 Add compose ui tooling dependency

--- formdsl/build.gradle.kts
+++ formdsl/build.gradle.kts
@@ -47,8 +47,9 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
 
     testImplementation(libs.junit)
 
